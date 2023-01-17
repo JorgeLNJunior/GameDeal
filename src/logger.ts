aa9063343dbce01599feed8e2765b27ea86e2f6c
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-01-17 16:27:19 -0300

    chore: criar uma classe logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,3 +1,23 @@
 import pino from 'pino'
 
-export default pino()
+export class Logger {
+  private pino = pino({
+    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug'
+  })
+
+  public info(obj: unknown, msg?: string, ...args: unknown[]) {
+    this.pino.info(obj, msg, args)
+  }
+
+  public error(obj: unknown, msg?: string, ...args: unknown[]) {
+    this.pino.error(obj, msg, args)
+  }
+
+  public fatal(obj: unknown, msg?: string, ...args: unknown[]) {
+    this.pino.fatal(obj, msg, args)
+  }
+
+  public warn(obj: unknown, msg?: string, ...args: unknown[]) {
+    this.pino.warn(obj, msg, args)
+  }
+}
