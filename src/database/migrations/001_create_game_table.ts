ce46b7cad49eadc86d976443019573ceba60dcd5
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-01-27 14:45:24 -0300

    fix: permitir valores nulos em nuuvem_url e green_man_gaming_url

--- src/database/migrations/001_create_game_table.ts
+++ src/database/migrations/001_create_game_table.ts
@@ -3,11 +3,12 @@ import { Kysely, sql } from 'kysely'
 export async function up(db: Kysely<unknown>): Promise<void> {
   await db.schema
     .createTable('game')
+    .ifNotExists()
     .addColumn('id', 'varchar(36)', (col) => col.primaryKey().notNull())
     .addColumn('title', 'varchar(255)', (col) => col.notNull().unique())
-    .addColumn('steam_url', 'varchar(500)', (col) => col.notNull())
-    .addColumn('nuuvem_url', 'varchar(500)', (col) => col.notNull())
-    .addColumn('green_man_gaming_url', 'varchar(500)', (col) => col.notNull())
+    .addColumn('steam_url', 'varchar(255)', (col) => col.notNull())
+    .addColumn('nuuvem_url', 'varchar(255)')
+    .addColumn('green_man_gaming_url', 'varchar(255)')
     .addColumn('created_at', 'timestamp', (col) =>
       col.notNull().defaultTo(sql`CURRENT_TIMESTAMP`)
     )
@@ -24,6 +25,6 @@ export async function up(db: Kysely<unknown>): Promise<void> {
 }
 
 export async function down(db: Kysely<unknown>) {
-  await db.schema.dropIndex('game_title_index').execute()
-  await db.schema.dropTable('game').execute()
+  await db.schema.dropIndex('game_title_index').ifExists().execute()
+  await db.schema.dropTable('game').ifExists().execute()
 }
