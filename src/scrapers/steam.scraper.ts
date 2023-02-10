cf50f26be636d747ea662a4fa7dfb02aa4e91a78
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-02-10 15:43:45 -0300

    fix: fechar a página do game scraper quando a promise retornar

--- src/scrapers/steam.scraper.ts
+++ src/scrapers/steam.scraper.ts
@@ -9,22 +9,30 @@ export class SteamScraper implements Scraper {
   constructor(private browser: Browser, private logger: Logger) {}
 
   async getGamePrice(url: string): Promise<number> {
+    this.logger.info('[SteamScraper] getting a page')
     const page = await this.browser.getPage()
 
-    this.logger.info(`Navigating to ${url}`)
-    await page.goto(url, { waitUntil: 'domcontentloaded' })
-    this.logger.info('waiting for page selector')
-    await page.waitForSelector('.game_area_purchase_game_wrapper')
+    try {
+      this.logger.info(`[SteamScraper] Navigating to ${url}`)
+      await page.goto(url, { waitUntil: 'domcontentloaded' })
 
-    const price = await page
-      .locator('div.game_area_purchase_game_wrapper .game_purchase_price')
-      .textContent()
+      this.logger.info('[SteamScraper] waiting for page selector')
+      await page.waitForSelector('.game_area_purchase_game_wrapper')
 
-    if (!price) {
-      this.logger.error(`No price found for ${url}`)
-      throw new Error('Game price not found')
+      const price = await page
+        .locator('div.game_area_purchase_game_wrapper .game_purchase_price')
+        .textContent()
+
+      if (!price) {
+        this.logger.error(`No price found for ${url}`)
+        throw new Error('Game price not found')
+      }
+
+      return Number(price.replace('R$', '').replace(',', '.').trim())
+    } finally {
+      this.logger.info('[SteamScraper] closing the page')
+      await page.close()
+      this.logger.info('[SteamScraper] page closed')
     }
-
-    return Number(price.replace('R$', '').replace(',', '.').trim())
   }
 }
