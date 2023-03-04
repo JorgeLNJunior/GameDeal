96f2f013f9c63a5b4e089c68992a718eb83b46d2
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-03-04 17:18:08 -0300

    test: adicionar suporte a nuuvem

--- src/modules/scrapers/nuuvem.scraper.spec.ts
+++ src/modules/scrapers/nuuvem.scraper.spec.ts
@@ -1,17 +1,17 @@
 import { Browser } from '../infra/browser'
 import { PinoLogger } from '../infra/pino.logger'
-import { SteamScraper } from './steam.scraper'
+import { NuuvemScraper } from './nuuvem.scraper'
 
 jest.setTimeout(30000)
 
-describe('SteamScraper', () => {
-  let scraper: SteamScraper
+describe('NuuvemScraper', () => {
+  let scraper: NuuvemScraper
   let browser: Browser
 
   beforeEach(async () => {
     const logger = new PinoLogger()
     browser = new Browser(logger)
-    scraper = new SteamScraper(browser, logger)
+    scraper = new NuuvemScraper(browser, logger)
     await browser.launch()
   })
   afterEach(async () => {
@@ -20,7 +20,7 @@ describe('SteamScraper', () => {
 
   it('should return a price', async () => {
     const price = await scraper.getGamePrice(
-      'https://store.steampowered.com/app/1593500/God_of_War'
+      'https://www.nuuvem.com/br-en/item/god-of-war'
     )
 
     expect(typeof price).toBe('number')
