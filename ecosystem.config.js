9caf3a227cd7e340d3f8c6b6e544c1f04e23d39c
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-04-04 14:40:45 -0300

    chore: não executar o pm2 em cluster mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,6 @@ module.exports = {
     {
       name: 'game-price-alert',
       script: 'dist/main.js',
-      instances: 'max',
-      exec_mode: 'cluster',
-      instance_var: 'INSTANCE_ID',
       env: {
         NODE_ENV: 'development'
       },
