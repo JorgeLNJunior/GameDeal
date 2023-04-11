35b06b452444ce2f3fa4145d75b91c46bafb9eca
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-04-11 13:25:06 -0300

    chore: alterar o nome do projeto

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'game-price-alert',
+      name: 'game-price-tracker',
       script: 'dist/main.js',
       env: {
         NODE_ENV: 'development'
