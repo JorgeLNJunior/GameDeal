ec78d60ed90244faa9dd7a786ca9ea198b5c9c9a
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-04-13 14:39:43 -0300

    feat: coletar os preços a cada 6 horas

--- src/modules/cron/jobs/gameScraping.cronjob.ts
+++ src/modules/cron/jobs/gameScraping.cronjob.ts
@@ -8,7 +8,7 @@ import { FindGameScraperDataRepository } from './repositories/findGameScraperDat
 
 @injectable()
 export class GameScrapingCronJob implements ApplicationCronJob {
-  public cronTime = '0 */2 * * *' // At minute 0 past every 2nd hour
+  public cronTime = '0 */6 * * *' // At minute 0 past every 6th hour
 
   /**
    * Handles the game scraping cron job.
