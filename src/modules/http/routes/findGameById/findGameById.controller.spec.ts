ab1524ddd716cd0f63e85ddfda6520fe157d9b72
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-04-17 14:25:16 -0300

    test: criar rota que retorna um jogo a partir do id

---
+++ src/modules/http/routes/findGameById/findGameById.controller.spec.ts
@@ -0,0 +1,53 @@
+import { FindGameByIdRepository } from '@modules/shared/repositories/findGameById.repository'
+import { container } from 'tsyringe'
+
+import { FindGameByIdController } from './findGameById.controller'
+
+describe('FindGameByIdController', () => {
+  let controller: FindGameByIdController
+  let repository: FindGameByIdRepository
+
+  beforeEach(async () => {
+    repository = container.resolve(FindGameByIdRepository)
+    controller = new FindGameByIdController(repository)
+  })
+
+  it('should return a game', async () => {
+    const game = {
+      id: 'id',
+      title: 'title',
+      steam_url: 'stem_url',
+      nuuvem_url: 'nuuvem_url',
+      created_at: new Date(),
+      updated_at: null
+    }
+    jest.spyOn(repository, 'find').mockResolvedValueOnce(game)
+
+    const response = await controller.handle({
+      body: {},
+      headers: {},
+      query: {},
+      params: { id: game.id }
+    })
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual(game)
+  })
+
+  it('should return 404 if the game was not found', async () => {
+    jest.spyOn(repository, 'find').mockResolvedValueOnce(undefined)
+
+    const response = await controller.handle({
+      body: {},
+      headers: {},
+      query: {},
+      params: { id: 'id' }
+    })
+
+    console.log(response.body)
+
+    expect(response.statusCode).toBe(404)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    expect((response.body as any).message).toBe('game not found')
+  })
+})
