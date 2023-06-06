31b2318cb4cfcd94709c4c1e3cca11ca900e3c4a
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-06-06 17:18:28 -0300

    test: criar a navbar

---
+++ apps/frontend/src/components/NavBar.spec.ts
@@ -0,0 +1,13 @@
+import { mount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+
+import { TELEGRAM_CHANNEL_URL } from '../constants/urls'
+import NavBar from './NavBar.vue'
+
+describe('NavBar', () => {
+  it('should contain a button that redirects to the telegram channel', async () => {
+    const wrapper = mount(NavBar)
+    const telegramButton = wrapper.get('[test-data="telegram_channel_btn"]')
+    expect(telegramButton.attributes().href).toBe(TELEGRAM_CHANNEL_URL)
+  })
+})
