93224ddd6f6b549fc1a3460a2a6ab8b743d1e2cf
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-06-16 14:49:56 -0300

    feat(front): criar o componente game search input

---
+++ apps/frontend/src/components/gameList/GameSearchInput.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup></script>
+
+<template>
+  <label class="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-cyan-600">
+    <input
+      type="email"
+      id="UserEmail"
+      placeholder="Email"
+      class="peer h-8 w-full border-none bg-transparent p-0 placeholder:text-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+    />
+
+    <span
+      class="absolute start-3 top-3 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs"
+    >
+      Titulo
+    </span>
+  </label>
+</template>
