19f1e54bed67849bbff71f6a1ba2dd8b141ac7c1
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-06-16 14:52:29 -0300

    chore(front): adicionar tipagem ao import env

---
+++ apps/frontend/src/types/env.d.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SERVER_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
