785cf4ea070cbcd62f1b65ff94b3d94bc7b047f1
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-06-19 19:28:39 -0300

    perf(front): importar a HomeView quando necessario

--- apps/frontend/src/router/index.ts
+++ apps/frontend/src/router/index.ts
@@ -1,20 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import HomeView from '../views/HomeView.vue'
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: async () => await import('@/views/HomeView.vue'),
       meta: { title: 'Home' }
     },
     {
       path: '/:pathMatch(.*)*',
       name: 'notFound',
-      component: async () => await import('../views/NotFound.vue'),
+      component: async () => await import('@/views/NotFound.vue'),
       meta: { title: '404 - Not Found' }
     }
   ]
