70c127433662c017a129c4de645778fd438685f8
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-06-21 14:31:28 -0300

    feat(front): limitar o tamanho do titulo dos jogos

--- apps/frontend/src/components/gameList/GameListItem.vue
+++ apps/frontend/src/components/gameList/GameListItem.vue
@@ -7,12 +7,15 @@ const props = defineProps({
   title: { type: String, required: true }
 })
 const priceWithCurrency = computed(() => `R$ ${props.price.replace('.', ',')}`)
+const cropedText = computed(() => {
+  return props.title.length > 33 ? (props.title.slice(0, 33) + '...') : props.title
+})
 </script>
 
 <template>
   <li>
-    <RouterLink class="group flex items-center justify-between rounded-lg px-4 py-2 hover:bg-gray-100 hover:text-gray-800" :to="`/games/${props.id}`">
-      <span test-data="title">{{ props.title }}</span>
+    <RouterLink class="group flex items-center justify-between rounded-lg px-4 py-2 hover:bg-gray-100 hover:text-gray-800" :to="`/game/${props.id}`">
+      <span test-data="title">{{ cropedText }}</span>
       <span test-data="price">{{ priceWithCurrency }}</span>
     </RouterLink>
   </li>
