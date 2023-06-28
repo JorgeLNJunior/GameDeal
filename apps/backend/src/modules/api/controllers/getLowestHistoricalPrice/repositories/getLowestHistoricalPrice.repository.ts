0410cf79db13577b8a087982497ce1bc4d7a7b6a
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-06-28 16:22:57 -0300

    fix(api): retornar o preço correto na rota de menor preço
    dois subselects sem where estavam filtrando o menor preço de forma incorreta

--- apps/backend/src/modules/api/controllers/getLowestHistoricalPrice/repositories/getLowestHistoricalPrice.repository.ts
+++ apps/backend/src/modules/api/controllers/getLowestHistoricalPrice/repositories/getLowestHistoricalPrice.repository.ts
@@ -12,11 +12,10 @@ export class GetLowestHistoricalPriceRepository {
       .raw<GamePrice>(
         `SELECT * FROM game_price
           WHERE game_id = "${gameID}"
-          AND steam_price = (SELECT MIN(steam_price) FROM game_price)
-            OR nuuvem_price = (SELECT MIN(nuuvem_price) FROM game_price)
+          AND steam_price = (SELECT MIN(steam_price) FROM game_price where game_id = "${gameID}")
+            OR nuuvem_price = (SELECT MIN(nuuvem_price) FROM game_price where game_id = "${gameID}")
           LIMIT 1;
-        `
-    )
+        `)
       .execute(this.databaseService.getClient())
     return data.rows.at(0)
   }
