c5e9596a9cb9acba1f642fbe0505a2318f4afeb2
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-07-27 15:29:08 -0300

    test: criar helper para aguardar

--- packages/testing/index.ts
+++ packages/testing/index.ts
@@ -1,4 +1,5 @@
 import { GameBuilder } from './builders/game.builder'
 import { GamePriceBuilder } from './builders/price.builder'
+import { wait } from './wait'
 
-export { GameBuilder, GamePriceBuilder }
+export { GameBuilder, GamePriceBuilder, wait }
---
+++ packages/testing/wait.ts
@@ -0,0 +1,5 @@
+export async function wait (ms: number): Promise<void> {
+  await new Promise((resolve) => {
+    setTimeout(() => { resolve(undefined) }, ms)
+  })
+}
