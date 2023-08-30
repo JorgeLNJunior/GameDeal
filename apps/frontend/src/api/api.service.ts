dbdc626508b1b4c67e419579c38315f1205df870
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-08-30 10:10:34 -0300

    fix(front): atualizar as rotas da api

--- apps/frontend/src/api/api.service.ts
+++ apps/frontend/src/api/api.service.ts
@@ -19,19 +19,19 @@ export class ApiService {
   }
 
   async getGamePrice (gameID: string): Promise<GamePrice> {
-    const response = await this.http.get(`/games/${gameID}/price`)
+    const response = await this.http.get(`/games/${gameID}/current`)
     return response.data as GamePrice
   }
 
   async getGamePriceHistory (gameID: string, page = 1, limit = 15): Promise<QueryData<GamePrice[]>> {
     const response = await this.http.get(
-      `/games/${gameID}/price/history?page=${page}&limit=${limit}&order=desc`
+      `/games/${gameID}/history?page=${page}&limit=${limit}&order=desc`
     )
     return response.data as QueryData<GamePrice[]>
   }
 
   async getLowestPrice (gameID: string): Promise<LowestPrice> {
-    const response = await this.http.get(`/games/${gameID}/price/lowest`)
+    const response = await this.http.get(`/games/${gameID}/lowest`)
     return response.data as LowestPrice
   }
 }
