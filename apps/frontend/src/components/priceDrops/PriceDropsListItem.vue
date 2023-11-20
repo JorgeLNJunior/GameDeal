13e7d2eac80dd66d50f29a17e4c444fdec3488d0
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-11-20 17:02:53 -0300

    style(front): corrigir erros de lint

--- apps/frontend/src/components/priceDrops/PriceDropsListItem.vue
+++ apps/frontend/src/components/priceDrops/PriceDropsListItem.vue
@@ -28,7 +28,7 @@ const getGame = async (): Promise<void> => {
   <li>
     <RouterLink v-if="game" class="flex content-end justify-between space-x-6 rounded-lg p-2 transition hover:bg-gray-100 hover:text-gray-800 md:px-4" :to="`/game/${game.id}`">
       <span class="line-clamp-1 w-full" test-data="title">{{ game.title }}</span>
-      <span class="md:w-18 line-clamp-1 w-1/4 shrink-0" test-data="price">{{ priceWithCurrency }}</span>
+      <span class="line-clamp-1 w-1/4 shrink-0 md:w-18" test-data="price">{{ priceWithCurrency }}</span>
     </RouterLink>
     <PriceDropsListItemSkeleton v-else test-data="item-skeleton" />
   </li>
