e0f857cac26e9c25f936197476fcc7867e593d82
JorgeLNJunior <jorgelnjunior@gmail.com>
2024-04-03 16:59:17 -0300

    fix(api): ordenar jogos de forma crescente se o título for nulo

--- apps/backend/src/modules/api/controllers/findGames/repositories/findGames.repository.ts
+++ apps/backend/src/modules/api/controllers/findGames/repositories/findGames.repository.ts
@@ -45,7 +45,7 @@ export class FindGamesRepository {
       query.title += '*' // see MySQL full text search boolean mode
       dbQuery = dbQuery.where(sql`MATCH`, sql`(title)`, sql`AGAINST (${query.title} IN BOOLEAN MODE)`)
     }
-    if (query.order === 'asc') dbQuery = dbQuery.orderBy('title', 'asc')
+    if (query.order === 'asc' || query.title == null) dbQuery = dbQuery.orderBy('title', 'asc')
     if (query.order === 'desc') dbQuery = dbQuery.orderBy('title', 'desc')
 
     const results = await dbQuery.execute()
