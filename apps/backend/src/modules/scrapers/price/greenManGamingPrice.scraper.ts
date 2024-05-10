b966f10456cfe226c0bd526cc3e62e182458fbde
JorgeLNJunior <jorgelnjunior@gmail.com>
2024-05-10 16:19:50 -0300

    fix(gmg): enviar o header Accept com o valor text/html

--- apps/backend/src/modules/scrapers/price/greenManGamingPrice.scraper.ts
+++ apps/backend/src/modules/scrapers/price/greenManGamingPrice.scraper.ts
@@ -15,13 +15,15 @@ export class GreenManGamingPriceScraper implements GamePriceScraper {
     @inject(CHEERIO_PARSER) private readonly parser: HTMLParser,
     @inject(PINO_LOGGER) private readonly logger: ApplicationLogger,
     private readonly axios: AxiosService
-  ) {}
+  ) { }
 
   async getGamePrice (gameUrl: string): Promise<number | null> {
     const data = await this.axios.get<string>(gameUrl, {
       headers: {
         // Request Brazilian prices.
-        Cookie: 'CountryKey=BR; GlobalCacheKey=LoggedOut:BR:VIP Visitor GroupFalse'
+        Cookie: 'CountryKey=BR; GlobalCacheKey=LoggedOut:BR:VIP Visitor GroupFalse',
+        Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0'
       }
     })
 
