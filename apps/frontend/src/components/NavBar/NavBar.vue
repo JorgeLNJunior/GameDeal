159422bf93966afe8176e15bb176e55b3d1b6f70
Jorge Junior <jorgelnjunior@gmail.com>
2025-04-27 19:52:03 -0300

    chore(ui): adicionar uma navbar

---
+++ apps/frontend/src/components/NavBar/NavBar.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { cn } from '@/lib/utils'
+import { Input } from '../ui/input'
+import NavBarLink from './NavBarLink.vue';
+</script>
+
+<template>
+  <div class="border-b">
+    <div class="flex h-16 items-center px-6">
+      <RouterLink to="/">
+        <img src="/logo.png" class="w-8" />
+      </RouterLink>
+      <nav :class="cn('flex mx-6 items-center space-x-4 lg:space-x-6', $attrs.class ?? '')">
+        <NavBarLink to="/" title="Início" />
+        <NavBarLink to="/deals" title="Promoções" />
+      </nav>
+      <div class="ml-auto flex items-center space-x-4">
+        <Input type="search" placeholder="Buscar..." class="md:w-[100px] lg:w-[300px]" />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ apps/frontend/src/components/NavBar/NavBarLink.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+const props = defineProps({
+  to: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <RouterLink :to="props.to" class="text-sm font-medium transition-colors hover:text-primary"
+    :class="{ 'text-muted-foreground': route.path !== props.to }">
+    {{ props.title }}
+  </RouterLink>
+</template>
