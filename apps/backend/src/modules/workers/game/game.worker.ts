da082711094ed2f2ef87db4cc817a108cbf63741
Jorge Junior <jorgelnjunior@gmail.com>
2026-07-05 18:29:44 -0300

    chore(workers): limit how much jobs a worker can run in parallel

--- apps/backend/src/modules/workers/game/game.worker.ts
+++ apps/backend/src/modules/workers/game/game.worker.ts
@@ -1,15 +1,15 @@
-import ConfigService from '@config/config.service'
-import { PINO_LOGGER } from '@dependencies/dependency.tokens'
-import { ApplicationLogger } from '@localtypes/logger.type'
-import { type GamePriceScraperData, QueueName } from '@localtypes/queue.type'
-import { Worker } from 'bullmq'
-import { inject, singleton } from 'tsyringe'
+import ConfigService from "@config/config.service";
+import { PINO_LOGGER } from "@dependencies/dependency.tokens";
+import { ApplicationLogger } from "@localtypes/logger.type";
+import { type GamePriceScraperData, QueueName } from "@localtypes/queue.type";
+import { Worker } from "bullmq";
+import { inject, singleton } from "tsyringe";
 
-import { GameJobProcessor } from './game.job.processor'
+import { GameJobProcessor } from "./game.job.processor";
 
 @singleton()
 export class GameWorker {
-  private worker!: Worker<GamePriceScraperData>
+  private worker!: Worker<GamePriceScraperData>;
 
   /**
    * Handles the game worker.
@@ -17,11 +17,11 @@ export class GameWorker {
    * @param config - An instance of `ConfigService`.
    * @param logger - An instance of `ApplicationLogger`.
    */
-  constructor (
+  constructor(
     private readonly gameJobProcessor: GameJobProcessor,
     private readonly config: ConfigService,
-    @inject(PINO_LOGGER) private readonly logger: ApplicationLogger
-  ) {}
+    @inject(PINO_LOGGER) private readonly logger: ApplicationLogger,
+  ) { }
 
   /**
    * Starts the game worker.
@@ -30,29 +30,39 @@ export class GameWorker {
    * await worker.init()
    * ```
    */
-  async init (): Promise<void> {
+  async init(): Promise<void> {
     this.worker = new Worker<GamePriceScraperData, void>(
       QueueName.GAME_PRICE_SCRAPING,
       async (job) => {
-        this.logger.info(job.data, `[GameWorker] processing job ${job.id ?? 'unknow'}`)
-        await this.gameJobProcessor.scrapePrice(job.data)
-        this.logger.info(`[GameWorker] job ${job.id ?? 'unknow'} processed`)
+        this.logger.info(
+          job.data,
+          `[GameWorker] processing job ${job.id ?? "unknow"}`,
+        );
+        await this.gameJobProcessor.scrapePrice(job.data);
+        this.logger.info(`[GameWorker] job ${job.id ?? "unknow"} processed`);
       },
       {
         connection: {
-          host: this.config.getEnv('REDIS_HOST'),
-          port: this.config.getEnv('REDIS_PORT'),
-          password: this.config.getEnv('REDIS_PASSWORD')
-        }
-      }
-    )
+          host: this.config.getEnv("REDIS_HOST"),
+          port: this.config.getEnv("REDIS_PORT"),
+          password: this.config.getEnv("REDIS_PASSWORD"),
+        },
+        limiter: {
+          max: 3,
+          duration: 1000,
+        },
+      },
+    );
 
-    this.worker.on('failed', (job, err) => {
-      this.logger.error(err, `[GameWorker] job "${job?.id ?? 'unknow'}" failed`)
-    })
-    this.worker.on('error', (err) => {
-      this.logger.error(err, '[GameWorker] worker failed')
-    })
+    this.worker.on("failed", (job, err) => {
+      this.logger.error(
+        err,
+        `[GameWorker] job "${job?.id ?? "unknow"}" failed`,
+      );
+    });
+    this.worker.on("error", (err) => {
+      this.logger.error(err, "[GameWorker] worker failed");
+    });
   }
 
   /**
@@ -62,9 +72,9 @@ export class GameWorker {
    * await worker.stop()
    * ```
    */
-  async stop (): Promise<void> {
-    this.logger.info('[GameWorker] stopping worker')
-    await this.worker.close()
-    this.logger.info('[GameWorker] worker stopped')
+  async stop(): Promise<void> {
+    this.logger.info("[GameWorker] stopping worker");
+    await this.worker.close();
+    this.logger.info("[GameWorker] worker stopped");
   }
 }
