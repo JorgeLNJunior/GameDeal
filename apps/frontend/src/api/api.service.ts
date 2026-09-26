6c116686e8f829bbbac36286d976683af9e6dfff
Jorge Junior <jorgelnjunior@gmail.com>
2026-09-26 16:16:03 -0300

    chore(env): change the way slashes are handled on web requests

--- apps/frontend/src/api/api.service.ts
+++ apps/frontend/src/api/api.service.ts
@@ -6,43 +6,44 @@ import { SERVER_URL } from '@/constants/urls'
 export class ApiService {
   private readonly http = axios.create({ baseURL: SERVER_URL })
 
-  async getGames (title?: string, page = 1, limit = 20): Promise<QueryData<Game[]>> {
-    let url = `/games?page=${page}&limit=${limit}`
+  async getGames(title?: string, page = 1, limit = 20): Promise<QueryData<Game[]>> {
+    let url = `games?page=${page}&limit=${limit}`
     if (title != null) url += `&title=${title}`
     const response = await this.http.get(url)
+    console.debug('BASE: ', SERVER_URL, ' FINAL: ', response.config.url)
     return response.data as QueryData<Game[]>
   }
 
-  async getGameByID (gameID: string): Promise<Game> {
-    const response = await this.http.get(`/games/${gameID}`)
+  async getGameByID(gameID: string): Promise<Game> {
+    const response = await this.http.get(`games/${gameID}`)
     return response.data as Game
   }
 
-  async getGamePrice (gameID: string): Promise<GamePrice> {
-    const response = await this.http.get(`/games/${gameID}/current`)
+  async getGamePrice(gameID: string): Promise<GamePrice> {
+    const response = await this.http.get(`games/${gameID}/current`)
     return response.data as GamePrice
   }
 
-  async getGamePriceHistory (gameID: string, page = 1, limit = 15): Promise<QueryData<GamePrice[]>> {
+  async getGamePriceHistory(gameID: string, page = 1, limit = 15): Promise<QueryData<GamePrice[]>> {
     const response = await this.http.get(
-      `/games/${gameID}/history?page=${page}&limit=${limit}&order=desc`
+      `games/${gameID}/history?page=${page}&limit=${limit}&order=desc`
     )
     return response.data as QueryData<GamePrice[]>
   }
 
-  async getLowestPrice (gameID: string): Promise<LowestPrice> {
-    const response = await this.http.get(`/games/${gameID}/lowest`)
+  async getLowestPrice(gameID: string): Promise<LowestPrice> {
+    const response = await this.http.get(`games/${gameID}/lowest`)
     return response.data as LowestPrice
   }
 
-  async getTodayPriceDrops (limit = 19): Promise<QueryData<GamePriceDrop[]>> {
+  async getTodayPriceDrops(limit = 19): Promise<QueryData<GamePriceDrop[]>> {
     const today = new Date().toISOString()
-    const response = await this.http.get(`/drops?date=${today}&limit=${limit}`)
+    const response = await this.http.get(`drops?date=${today}&limit=${limit}`)
     return response.data as QueryData<GamePriceDrop[]>
   }
 
-  async getGamesCount (): Promise<number> {
-    const response = await this.http.get('/games/count')
+  async getGamesCount(): Promise<number> {
+    const response = await this.http.get('games/count')
     return response.data.total as number
   }
 }
